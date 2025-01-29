f3e6b940d021d2692afb2a6f3ee98c80627fc8b3
Tomasz Polski <tomaszpolski21@gmail.com>
2025-01-29 17:53:06 +0100

    modified:   foo.hpp

--- foo.hpp
+++ foo.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "Human.hpp"
-
 #include <algorithm>
 #include <list>
 #include <vector>
@@ -9,10 +8,14 @@
 std::vector<char> foo(std::list<Human>& people) {
     std::vector<char> retval(people.size());
 
+    // Inkrementacja wieku wszystkich obiektów
     std::for_each(people.begin(), people.end(), [](Human& h) { h.birthday(); });
 
-    std::transform(people.rbegin(), people.rend(), retval.begin(), 
-                   [](const Human& h) { return h.isMonster() ? 'n' : 'y'; });
+    // Przypisanie wartości do wektora wynikowego bez lambdy w transform
+    auto it = retval.rbegin();
+    std::for_each(people.begin(), people.end(), [&it](const Human& h) {
+        *it++ = h.isMonster() ? 'n' : 'y';
+    });
 
     return retval;
 }
